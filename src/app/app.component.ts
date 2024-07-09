ab8303e1ed070196b70816994252ece7fd90f7c3
TheoCaesar <Theocaesar7@gmail.com>
2024-07-09 08:59:04 +0000

    Exercise 1

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,19 +1,29 @@
-import { Component } from '@angular/core';
+import { Component, output } from '@angular/core';
 import { HeaderComponent } from './header/header.component';
 import { UserComponent } from './user/user.component';
 import { DUMMY_USERS } from './dummy-users';
+import { TasksComponent } from './tasks/tasks.component';
+export interface user {
+  id: string,
+  name: string,
+  avatar: string
+}
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [HeaderComponent, UserComponent],
+  imports: [HeaderComponent, UserComponent, TasksComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'first-angular-app2';
   users = DUMMY_USERS;
+  taskInfo !: any ;
 
   onSelectUSER(idArg:string) {
-    console.log("Selected user with id " + idArg)
+    this.taskInfo = this.users.find(user => user.id == idArg)!
+    console.log("Selected user id" + this.taskInfo)
+    console.log("Selected user id" + this.taskInfo)
+
   }
 }
