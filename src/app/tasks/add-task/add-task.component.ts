eda64e71672606f4758c0d8a4819b693d94378ec
TheoCaesar <Theocaesar7@gmail.com>
2024-07-12 06:56:10 +0000

    Using ngModel Directive - 2 way binding

--- src/app/tasks/add-task/add-task.component.ts
+++ src/app/tasks/add-task/add-task.component.ts
@@ -1,14 +1,18 @@
 import { Component, EventEmitter, Output } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-add-task',
   standalone: true,
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './add-task.component.html',
   styleUrl: './add-task.component.css'
 })
 export class AddTaskComponent {
   @Output() cancel = new EventEmitter()
+  taskTitle :string = ""
+  taskSummary :string = "";
+  taskDate :string = "";
 
   cancelNewTask() {
     this.cancel.emit()
